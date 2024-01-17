ada90798f91f4017f8bee48968fdb84bb7b8a141
75WAY <75way@75WAYs-Mac-mini.local>
2024-01-17 18:12:11 +0530

    3rd commit

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ try {
     console.log(error);
 }
 cron.schedule('0 18 * * * *',sendCronMail,{timezone:'Asia/Kolkata'});
-cron.schedule('* * 30 * * *',lowQuantityNotification,{timezone:'Asia/Kolkata'});
+cron.schedule('* 30 * * * *',lowQuantityNotification,{timezone:'Asia/Kolkata'});
 
 app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp/' }))
 app.use('/user', authRoutes);
